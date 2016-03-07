1bdb272582b59ee053121b8ab09265a5be274246
joern <Gerrit@kaiMac.local>
2016-03-07 18:37:22 +0100

    dropping blocks

--- DropIt/DropIt/ViewController.m
+++ DropIt/DropIt/ViewController.m
@@ -9,10 +9,77 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *gameView;
+@property (strong, nonatomic) UIDynamicAnimator *animator;
+@property (strong, nonatomic) UIGravityBehavior *gravity;
+@property (strong, nonatomic) UICollisionBehavior *collider;
 
 @end
 
 @implementation ViewController
+
+static const CGSize DROP_SIZE = { 40, 40 };
+
+- (UIDynamicAnimator *)animator{
+    if(!_animator){
+        _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.gameView];
+    }
+    return _animator;
+}
+
+- (UIGravityBehavior *)gravity{
+    
+    if(!_gravity){
+        _gravity = [[UIGravityBehavior alloc] init];
+        _gravity.magnitude = 0.9; // somewhat gravity acceleration where 1.0 is approx. like real life gravity acceleration
+        [self.animator addBehavior:_gravity];
+    }
+    return _gravity;
+}
+
+- (UICollisionBehavior *)collider{
+    
+    if(!_collider){
+        _collider = [[UICollisionBehavior alloc] init];
+        _collider.translatesReferenceBoundsIntoBoundary = YES;
+        [self.animator addBehavior:_collider];
+    }
+    return _collider;
+}
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender {
+    
+    [self drop];
+    
+}
+- (void)drop{
+    
+    CGRect frame;
+    frame.origin = CGPointZero;
+    frame.size = DROP_SIZE;
+    int x = (arc4random() % (int)self.gameView.bounds.size.width) / DROP_SIZE.width;
+    frame.origin.x = x * DROP_SIZE.width;
+    
+    UIView *dropView = [[UIView alloc] initWithFrame:frame];
+    dropView.backgroundColor = [self randomColor];
+    [self.gameView addSubview:dropView];
+    
+    [self.gravity addItem:dropView];
+    [self.collider addItem:dropView];
+    
+}
+
+- (UIColor *)randomColor{
+    switch(arc4random()%5){
+        case 0: return [UIColor greenColor];
+        case 1: return [UIColor blueColor];
+        case 2: return [UIColor orangeColor];
+        case 3: return [UIColor redColor];
+        case 4: return [UIColor purpleColor];
+        default: return [UIColor blackColor];
+            
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
